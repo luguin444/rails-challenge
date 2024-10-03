c16b0c6f4a12369a59d51d7159bfff07c95139f2
Luis Gustavo <luguin444@gmail.com>
2024-10-02 21:00:48 -0300

    chore: adapt into use case architecture

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -18,19 +18,9 @@ class ProductsController < ApplicationController
   end
 
   def index
-    products_filtered_and_sorted = Product.ransack(params[:q]).result
-    products_serialized = products_filtered_and_sorted.select(:id, :name, :price, :expiration, :code, :upload_file_id)
+    filter_and_sort_params = params[:q]
 
-    upload_file_ids = products_serialized.map(&:upload_file_id)
-    currencies_rates = CurrencyRate.includes(:currency).where(upload_file_id: upload_file_ids)
-
-    products_with_currency_rates = products_serialized.map do |product|
-      product.as_json.merge({
-        conversion_rates: currencies_rates
-        .select { |rate| rate.upload_file_id == product.upload_file_id }
-        .map { |rate| { symbol: rate.currency.symbol, name: rate.currency.name, rate: rate.rate } }
-      })
-    end
+    products_with_currency_rates = Products::List::GetSerializedProducts.new(filter_and_sort_params).exec
 
     render json: { products: products_with_currency_rates }, status: :ok
   end
---
+++ app/useCases/products/list/get_serialized_products.rb
@@ -0,0 +1,29 @@
+class Products::List::GetSerializedProducts
+  def initialize(filter_and_sort_params)
+    @filter_and_sort_params = filter_and_sort_params
+  end
+
+  def exec
+    products_filtered_and_sorted = Product.ransack(@filter_and_sort_params).result
+    upload_file_ids = products_filtered_and_sorted.map(&:upload_file_id)
+
+    currencies_rates = CurrencyRate.includes(:currency).where(upload_file_id: upload_file_ids)
+
+    products_with_currency_rates = serialize(products_filtered_and_sorted, currencies_rates)
+    products_with_currency_rates
+  end
+
+  def serialize(products, currencies_rates)
+    products_serialized = products.select(:id, :name, :price, :expiration, :code, :upload_file_id)
+
+    products_with_currency_rates = products_serialized.map do |product|
+      product.as_json.merge({
+        conversion_rates: currencies_rates
+        .select { |rate| rate.upload_file_id == product.upload_file_id }
+        .map { |rate| { symbol: rate.currency.symbol, name: rate.currency.name, rate: rate.rate } }
+      })
+    end
+
+    products_with_currency_rates
+  end
+end
